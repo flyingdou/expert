546e3017cd711a3cad6da8870b9f79d2cb4a8b09
w1441639547 <1441639547@qq.com>
2018-07-10 10:00:20 +0800

    优惠券页面添加下拉刷新功能

--- pages/ticket/ticket.js
+++ pages/ticket/ticket.js
@@ -1,3 +1,4 @@
+var app = getApp();
 Page({
 
   /**
@@ -12,49 +13,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var obj = this;
-    var memberId = wx.getStorageSync("memberId");
-    wx.request({
-      url: 'https://www.ecartoon.com.cn/expertex!findTicketByType.asp',
-      data: {
-        memberId: memberId
-      },
-      success: function(res) {
-        res.data.tickets.forEach(function(item){
-          var scope = '';
-          var types = item.scope.split(",");
-          types.forEach(function(typeItem){
-            typeItem = parseInt(typeItem);
-            if(typeItem == 1){
-              scope += '健身卡,';
-            } else if (typeItem == 2){
-              scope += '健身挑战,';
-            } else if (typeItem == 3) {
-              scope += '健身计划,';
-            } else if (typeItem == 4) {
-              scope += '场地预定,';
-            } else if (typeItem == 5) {
-              scope += '团体课程,';
-            } else if (typeItem == 6) {
-              scope += '智能计划,';
-            } else if (typeItem == 7) {
-              scope += '健身E卡通,';
-            }
-          });
-          if(scope.length > 18){
-            scope = scope.substring(0, 18);
-            scope += '...';
-          } else {
-            scope = scope.substring(0, scope.length - 1);
-          }
-          item.scope = scope;
-        });
-        // 设置数据源
-        obj.setData({
-          tickets: res.data.tickets
-        });
-      }
-    })
+
   },
 
   /**
@@ -68,49 +27,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    var obj = this;
-    var memberId = wx.getStorageSync("memberId");
-    wx.request({
-      url: 'https://www.ecartoon.com.cn/expertex!findTicketByType.asp',
-      data: {
-        memberId: memberId
-      },
-      success: function (res) {
-        res.data.tickets.forEach(function (item) {
-          var scope = '';
-          var types = item.scope.split(",");
-          types.forEach(function (typeItem) {
-            typeItem = parseInt(typeItem);
-            if (typeItem == 1) {
-              scope += '健身卡,';
-            } else if (typeItem == 2) {
-              scope += '健身挑战,';
-            } else if (typeItem == 3) {
-              scope += '健身计划,';
-            } else if (typeItem == 4) {
-              scope += '场地预定,';
-            } else if (typeItem == 5) {
-              scope += '团体课程,';
-            } else if (typeItem == 6) {
-              scope += '智能计划,';
-            } else if (typeItem == 7) {
-              scope += '健身E卡通,';
-            }
-          });
-          if (scope.length > 18) {
-            scope = scope.substring(0, 18);
-            scope += '...';
-          } else {
-            scope = scope.substring(0, scope.length - 1);
-          }
-          item.scope = scope;
-        });
-        // 设置数据源
-        obj.setData({
-          tickets: res.data.tickets
-        });
-      }
-    })
+    // 页面初始化(获取优惠券数据)
+    this.methods.init(this);
   },
 
   /**
@@ -131,7 +49,10 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+    // 页面初始化(获取优惠券数据)
+    this.methods.init(this);
+    // 一秒后结束下拉
+    setTimeout(e => wx.stopPullDownRefresh(), 1000);
   },
 
   /**
@@ -165,6 +86,59 @@ Page({
      wx.navigateTo({
        url: '../../pages/activeTicket/activeTicket',
      })
-  }
+  },
 
+
+  /**
+   * 自定义函数
+   */
+  methods: {
+    /**
+     * 页面初始化
+     */
+    init: function (obj) {
+      var memberId = wx.getStorageSync("memberId");
+      wx.request({
+        url: app.request_url + 'findTicketByType.asp',
+        data: {
+          memberId: memberId
+        },
+        success: function (res) {
+          res.data.tickets.forEach(function (item) {
+            var scope = '';
+            var types = item.scope.split(",");
+            types.forEach(function (typeItem) {
+              typeItem = parseInt(typeItem);
+              if (typeItem == 1) {
+                scope += '健身卡,';
+              } else if (typeItem == 2) {
+                scope += '健身挑战,';
+              } else if (typeItem == 3) {
+                scope += '健身计划,';
+              } else if (typeItem == 4) {
+                scope += '场地预定,';
+              } else if (typeItem == 5) {
+                scope += '团体课程,';
+              } else if (typeItem == 6) {
+                scope += '智能计划,';
+              } else if (typeItem == 7) {
+                scope += '健身E卡通,';
+              }
+            });
+            if (scope.length > 18) {
+              scope = scope.substring(0, 18);
+              scope += '...';
+            } else {
+              scope = scope.substring(0, scope.length - 1);
+            }
+            item.scope = scope;
+          });
+          // 设置数据源
+          obj.setData({
+            tickets: res.data.tickets
+          });
+        }
+      })
+    }
+  }
 })
